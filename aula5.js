6988a478b08c6d062d8b470aa38326e1bfe7f45c
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2021-08-30 22:29:45 -0300

    Add files via upload

---
+++ aula5.js
@@ -0,0 +1,36 @@
+/** letra A */
+var array =[2,3,5,6];
+console.log(array [1]);
+/**letra B */
+/**Altura de um array */
+array[0]= 10;
+console.log(array[0]);
+/**Letra C */
+array.push(100);
+console. log(array);
+ /**Letra D */
+ array.pop()
+ console.log (array);
+  /**Letra E */
+  if (array [0] = newarray [0]){
+      console.log("São Iguais")
+  } else{
+      console.log("Deu Pau")
+  }
+  /**Imprimir inverso */
+  function imprimirInverso(array){
+      console.log(array[3]);
+      console.log(array[3], array[2], array[1], array[0]);
+  }
+  imprimirInverso(array);
+  /**Função inverse e inverte o array */
+  function imprimirInverso(array){
+      console.log(array.reverse());
+  }
+  function inverter (array){
+      let newarray =[]
+      let newarray = array.reverse ();
+      console.log(newarray);
+  }
+  imprimirInverso(array);
+  inverter(newarray);
